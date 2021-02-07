2941b8e20e8e155d592881229fff1f8cce931cea
최윤정 <jjjung1003@naver.com>
2021-02-07 17:15:02 +0900

    토스트 출력하기

--- app/src/main/java/com/thejoeun/kotlinbasic_20210207/MainActivity.kt
+++ app/src/main/java/com/thejoeun/kotlinbasic_20210207/MainActivity.kt
@@ -3,6 +3,7 @@ package com.thejoeun.kotlinbasic_20210207
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,12 +16,18 @@ class MainActivity : AppCompatActivity() {
 //            ctrl + / 로 이 줄을 주석으로 만듬.
 //            sendBtn이 눌리면 실행해줄 코드.
             Log.d("메인화면", "전송버튼 눌림")
+
+            Toast.makeText(this,"전송버튼 클릭됨",Toast.LENGTH_SHORT).show()
+
         }
 
 //        로그 버튼이 눌리면 할 일
         logBtn.setOnClickListener {
             Log.d("메인화면","로그버튼 눌림")
             Log.e("메인화면","실행되면 안되는데 실행되는지 체크")
+
+//            로그버튼 눌림 토스트 출력
+            Toast.makeText(this,"로그버튼 눌림", Toast.LENGTH_SHORT).show()
         }
 
 
