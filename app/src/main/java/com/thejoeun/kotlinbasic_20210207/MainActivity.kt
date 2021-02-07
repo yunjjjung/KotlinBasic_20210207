930055815345dc9292f5567682fb8bac31224bd0
최윤정 <jjjung1003@naver.com>
2021-02-07 16:43:33 +0900

    버튼 추가. 그 버튼만의 이벤트 따로 할당

--- app/src/main/java/com/thejoeun/kotlinbasic_20210207/MainActivity.kt
+++ app/src/main/java/com/thejoeun/kotlinbasic_20210207/MainActivity.kt
@@ -10,13 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        전송버튼이 눌렷을 때 할 일
         sendBtn.setOnClickListener {
 
 //            ctrl + / 로 이 줄을 주석으로 만듬.
 //            sendBtn이 눌리면 실행해줄 코드.
             Log.d("메인화면", "전송버튼 눌림")
+        }
 
-
+//        로그 버튼이 눌리면 할 일
+        logBtn.setOnClickListener {
+            Log.d("메인화면","로그버튼 눌림")
         }
 
 
