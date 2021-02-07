8761720e8c1c943ffa1d4e26761bb4fd7f004ce4
최윤정 <jjjung1003@naver.com>
2021-02-07 16:38:16 +0900

    전송버튼 누르면, 로그 남기기

--- app/src/main/java/com/thejoeun/kotlinbasic_20210207/MainActivity.kt
+++ app/src/main/java/com/thejoeun/kotlinbasic_20210207/MainActivity.kt
@@ -2,12 +2,27 @@ package com.thejoeun.kotlinbasic_20210207
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sendBtn.setOnClickListener {
+
+//            ctrl + / 로 이 줄을 주석으로 만듬.
+//            sendBtn이 눌리면 실행해줄 코드.
+            Log.d("메인화면", "전송버튼 눌림")
+
+
+        }
+
+
+
+
+
 
     }
 }
